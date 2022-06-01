553fa00100516cfa8715db3aac71e498bf9ed83e
Philip <philipcho101@hotmail.com>
2022-05-31 20:21:29 -0700

    Add display controller function

--- script.js
+++ script.js
@@ -17,6 +17,24 @@ const Player = (name, marker) => {
     return {name, marker}
 };
 
+const displayController = (() => {
+    const gridSize = 3;
+    const gameGrid = document.querySelector('.game-grid');
+    gameGrid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    gameGrid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        let grid = document.createElement('div');
+        grid.classList.add('grid');
+        grid.id = `${i}`;
+        grid.textContent = 'x';
+        gameGrid.appendChild(grid);
+    }
+    return {
+        
+    };
+})();
+
 const gameController = (() => {
     const player1 = Player('p1', 'x');
     const player2 = Player('p2', 'o');
