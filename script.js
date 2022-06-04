5e0bdfd48833b7aa940e50e1b0dbea9acac61e7b
Philip <philipcho101@hotmail.com>
2022-06-03 22:23:57 -0700

    Add display for winner and current turn

--- script.js
+++ script.js
@@ -35,7 +35,9 @@ const displayController = (() => {
     const playerName1 = document.querySelector('.player-1-name');
     const playerName2 = document.querySelector('.player-2-name');
     const resetBtn = document.querySelector('.game-reset');
-    
+    const gameDisplay = document.querySelector('.game-display');
+    const turnDisplay = document.querySelector('.turn-display');
+
     for (let i = 0; i < grids.length; i++) {
         grids[i].addEventListener('click', placeMarker);
     }
@@ -45,9 +47,24 @@ const displayController = (() => {
             gameBoard.setMarker(e.target.id, mark);
             e.target.textContent = gameBoard.getMarker(e.target.id);
             gameController.checkWinner();
-            gameController.switchTurns();
+            if (gameController.isGameOver()) {
+                displayWinner(gameController.getWinner());
+            }
+            else {
+                gameController.switchTurns();
+                turnDisplay.textContent = `It is ${gameController.getCurrentPlayer().name}'s move (${gameController.getCurrentPlayer().marker}) pieces`;
+            }
         }
     }
+    const displayWinner = (winner) => {
+        gameDisplay.textContent = 'Game Over! Click Reset Game to try again'
+        if (winner == undefined) {
+            turnDisplay.textContent = 'Tie Game';
+        }
+        else {
+            turnDisplay.textContent = `${winner.name} is the winner with the (${winner.marker}) pieces`;
+        }
+    };
 
     changeBtn1.addEventListener('click', () => {
         const newName = prompt('Enter new name for p1', 'p1');
@@ -65,6 +82,9 @@ const displayController = (() => {
         for (let i = 0; i < grids.length; i++) {
             grids[i].textContent = undefined;
         }
+        resetBtn.textContent = 'Reset Game';
+        gameDisplay.textContent = 'Tic Tac Toe Match';
+        turnDisplay.textContent = `It is ${gameController.getCurrentPlayer().name}'s move (${gameController.getCurrentPlayer().marker}) pieces`;
     });
 })();
 
@@ -72,7 +92,7 @@ const gameController = (() => {
     let player1 = Player('p1', 'x');
     let player2 = Player('p2', 'o');
     let currentPlayer = Math.floor(Math.random()*2) ? player1 : player2;
-    let gameOver = false;
+    let gameOver = true;
     let winner = undefined;
 
     console.log('New Game!');
@@ -99,25 +119,27 @@ const gameController = (() => {
             gameBoard.getMarker(2) == marker && gameBoard.getMarker(5) == marker && gameBoard.getMarker(8) == marker ||
             gameBoard.getMarker(0) == marker && gameBoard.getMarker(4) == marker && gameBoard.getMarker(8) == marker ||
             gameBoard.getMarker(2) == marker && gameBoard.getMarker(4) == marker && gameBoard.getMarker(6) == marker) {
-            console.log('Winner!');
             gameOver = true;
             winner = currentPlayer;
         }
-        if (gameBoard.isFull()) {
-            console.log('Tie!');
+        else if (gameBoard.isFull()) {
             gameOver = true;
-            winner = 'tie';
+            winner = undefined;
         }
+    };
+    const getWinner = () => {
+        return winner;
     };
     const switchTurns = () => {
         currentPlayer = currentPlayer == player1 ? player2 : player1;
     };
     const resetGame = () => {
         gameBoard.resetBoard();
+        currentPlayer = Math.floor(Math.random()*2) ? player1 : player2;
         gameOver = false;
         winner = undefined;
     };
     return {
-        setPlayerName1, setPlayerName2, getCurrentPlayer, isGameOver, checkWinner, switchTurns, resetGame,
+        setPlayerName1, setPlayerName2, getCurrentPlayer, isGameOver, checkWinner, getWinner, switchTurns, resetGame,
     };
 })();
